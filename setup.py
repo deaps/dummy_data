a57dd61b74098af5bd46d71720908a878ff9bb09
Joao Andrade <joaoandrade2112@gmail.com>
2016-06-16 23:21:05 +0200

    'first!'

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dummy_data',
+    version='0.1',
+    description='Create dummy data dynamically.',
+    url='http://github.com/deaps/dummy_data',
+    author='João Andrade',
+    author_email='joaoandrade2@protonmail.com',
+    license='MIT',
+    packages=['dummy_data'],
+    zip_safe=True
+)
